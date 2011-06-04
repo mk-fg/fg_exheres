b155be87d0f9bf4900d3a815e13f3b5592decea5
Mike Kazantsev <mk.fraggod@gmail.com>
2011-06-04 12:40:50 +0600

    +pyro +shinken

--- _hook_pre-commit.py
+++ _hook_pre-commit.py
@@ -248,7 +248,7 @@ def check_file(src, exheres=None):
 	for line in src:
 		if line.startswith('require'):
 			for mod in line.split():
-				if mod in ('sourceforge', 'gnu', 'gnome-2', 'launchpad', 'hackage')\
+				if mod in ('sourceforge', 'gnu', 'gnome-2', 'launchpad', 'hackage', 'pypi')\
 					or mod.startswith('scm-'): exlib_src = True
 				if mod == 'ejabberd-module': exlib_meta = True
 				if mod == 'hackage': exlib_deps = True
